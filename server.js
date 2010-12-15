e3cdebe92397aacdae57eec9fb73406d272d2101
tjarratt <tjarratt@gmail.com>
2010-12-14 16:35:11 -0800

    switched from grasshopper to express. Small amount of polish before node conf demo

--- server.js
+++ server.js
@@ -2,47 +2,59 @@ var sys = require("sys");
 __appRoot = __dirname;
 sys.puts("starting server with root: " + __appRoot);
 
-//setup web server
-var gh = require("grasshopper");
-gh.configure({
-  viewsDir : "./",
-});
+var express = require("express");
+var app = express.createServer(
+  express.compiler({src: __dirname, enable: ["sass"]}),
+  express.staticProvider(__dirname)
+);
 
-var cleanDirtyDirectories = function() {
-  var exec = require("child_process").exec;
-  exec("rm " + __appRoot + "/voices/*");
-}
+app.set("views", __dirname);
+app.set("view engine", "jade");
 
-//render index
-gh.get("/", function() {
-  this.render("/index");
-});
-
-//respond to /say
-gh.post("/say", function(args) {
-  var self = this;
-  
-  //could be a timing issue here
+app.get("/", function(req, res) {
+  console.log("AAAAH");
+  sys.puts("waaaha");
   process.nextTick(cleanDirtyDirectories);
+  res.render("index", {});
+});
+
+app.post("/say", function(request, response) {
+  var ajaxData = "/say?",
+      self = this;
   
-  ajaxData = "/say?";
-  var request = gh.request;
   request.on("data", function(chunk) {
+    console.log("got data chunk");
     ajaxData += chunk;
   });
   request.on("end", function() {
+    console.log("got all data");
     gotAllData(ajaxData);
   });
   
   var gotAllData = function(ajaxData) {        
-    require.paths.unshift(__appRoot + '/vendor/mongoose');
-    var mongoose = require('mongoose').Mongoose;
+    require.paths.unshift('vendor/mongodb');//TODO: parse this data
+    var mongoose = require(__appRoot + '/vendor/mongoose/mongoose').Mongoose;
+    mongoose.model("Voice", {
+      properties: ["id", "filepath", "text"],
+      cast: {
+        id: String,
+        filepath: String,
+        text: String,
+      },
+      indexes: ["id"],
+    });
+    var db = mongoose.connect("mongodb://localhost/db");
+    var Voice = db.model("Voice");
+    var thisVoice = new Voice();
     
     ajaxData = require("url").parse(ajaxData, true).query;
     
     var textToSpeak = ajaxData.say;
     var voice = ajaxData.voice;
     voice = voice? voice : "Vicki";
+    
+    var voiceModel = new Voice;
+    voiceModel.text = textToSpeak;
 
     textToSpeak = textToSpeak.replace(/[~`!@#$%^&*()_+-=":';?\/\\>.<,]/g, "");
     if (/^[A-Za-z0-9\s]*$/.test(textToSpeak) != true) {
@@ -57,12 +69,25 @@ gh.post("/say", function(args) {
     var exec = require("child_process").exec;
     var filename = "/voices/" + id + ".aiff";
     
+    voiceModel.id = id;
+    voiceModel.filepath = filename;
+    voiceModel.save();
+    
     exec("say -v " + voice + " -o " + __appRoot + filename + " " + textToSpeak, function(result) {
       //return file descriptor
       sys.puts("returning id:" + id + " and file: " + filename);
-      self.renderText(JSON.stringify({"id" : id, "filename" : filename}));
+      response.send(JSON.stringify({"id" : id, "filename" : filename}));
     });
   }
 });
 
-gh.serve(8080);
+app.listen(3000);
+console.log("Express app started on port 3000");
+
+var cleanDirtyDirectories = function() {
+  var exec = require("child_process").exec;
+  exec("rm " + __appRoot + "/voices/*", function(test) {
+    console.log(test);
+    console.log("did we delete all the old .aiff files?");
+  });
+}
